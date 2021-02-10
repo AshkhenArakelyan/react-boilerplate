a285378221f34520286810905905f9844b1ea74f
AshkhenArakelyan <ashkhenarakelyank@gmail.com>
2021-02-10 11:21:14 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Layout from "components/Layout/Layout";
+import Container from "containers/Container/Container";
+
+function App() {
+  return (
+    <div className="App">
+        <Layout>
+          <Container />
+        </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Container/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Container.scss';
+
+const Container = () => {
+    return (
+        <div className="app-container">
+            <h1 className="app-container__text">Container</h1>
+        </div>
+    )
+}
+
+export default Container;
